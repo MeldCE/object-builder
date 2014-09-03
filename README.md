4cd65912a756ea6aa2b006a66658aa5ede3b9d3e
Jack Farley <jack@meldce.com>
2014-09-03 01:59:36 -0500

    Got basic functionality working. Need to change the way the ObjectBuild is created and then interacted with.

---
+++ README.md
@@ -0,0 +1,122 @@
+object-builder
+==============
+
+ObjectBuilder allows the construction of a complex Javascript object using
+a GUI to put together elements given to the ObjectBuilder. It uses jQuery to
+create the GUI used to build the object.
+
+When a ObjectBuilder is created, the elements are placed in the ObjectBuilder
+store div separated into different types. A user can then click and drag these
+elements onto the pad to add them to the created object. Each element can have
+their own pads which more elements can be placed on, creating a hierarchical
+object.
+
+The Javascript object is created by going through the elements in the pad.
+Drawing and parsing functions can be specified for each element or element
+type. If no parsing functions are specified for an element, an object will
+created with the element type, element id and element value to represent
+that element.
+
+To initiate, simply call `ObjectBuilder.create(div, elements, options)`
+- `div`      - The div that the Object Builder will be placed in.
+- `elements` - An object containing the elements available. This should the
+               parameters as given below.
+- `options` - See Options below.
+
+== Elements ==
+The elements object passed to the ObjectBuilder should contain element
+types containing elements to be used to build the object.
+
+=== Element Types ===
+Each element type should be a unique object within the elements object and
+can contain the following properties:
+- `label: <string>` - Label to be used for the heading of the element types
+  in the store
+- `className: <string>` - Class to be put on each element
+- `draw: <function>` - Function to be used to draw an element of this type
+  when placed in the pad
+- `parse: <function>` - Function to be used to parse an element of this type
+
+=== Elements ===
+Each element should be a unique object within the element type object and
+must contain the following properties:
+- `label: <string>` - Label to be used for the heading of the element types
+  in the store
+
+Each element can also contain the following properties:
+- `value: <value>` - If a draw and parse function is not specified, this
+  value will be added to these elements when the object is parsed.
+- `className: <string>` - Class to be put on these elements.
+- `draw: <function>` - Function to be used to draw these elements
+  when placed in the pad.
+- `parse: <function>` - Function to be used to parse these elements.
+
+== Options == 
+The following options are available:
+- `input: <jQueryObject>` - Input element to place the parsed object JSON into
+  every time the object is updated
+- `onchange: <function>` - Function that will be called every time the object
+  is updated. It will be passed the ObjectBuilder id as an argument.
+
+== Example ==
+```
+<script language="JavaScript" type="text/javascript">
+	(function() {
+		function drawJoin(obj, id) {
+			console.log('Drawing Join');
+			
+			obj.html('Join ');
+
+			// Add pad
+			obj.data({pad: ObjectBuilder.createPad(id, obj, { multiple: true })});
+		}
+
+		function parseJoin(type, element, obj, id) {
+			var data;
+			if (data = obj.data()) {
+				return {
+					parts: ObjectBuilder.parsePadObject(id, data.pad)
+				}
+			}
+		}
+
+		elements = {
+			fields: {
+				label: 'Fields',
+				className: 'field',
+				elements: {
+					title: {
+						label: 'Title'
+					},
+					first_name: {
+						label: 'First Name',
+					},
+					last_name: {
+						label: 'Last Name',
+					},
+					email: {
+						label: 'Email',
+					},
+				},
+			},
+			functions: {
+				label: 'Functions',
+				className: 'function',
+				elements: {
+					CONCAT: {
+						label: 'Join',
+						draw: drawJoin,
+						parseObject: parseJoin
+					},
+				},
+			},
+		};
+
+		function alertObject(id) {
+			alert('The object is now\n' + ObjectBuilder.getObjectJSON(id));
+		}
+
+		ObjectBuilder.create(div, elements, { input: input, onchange: alertObject });
+	})();
+</script>
+```
--- objectBuilder.js
+++ objectBuilder.js
@@ -1,89 +1,269 @@
-var ObjectBuilder = (function() {
-	return function(obj, elements, options) {
-		var id = (new Date().getTime()).toString(16);
-		var builders = {};
+var ObjectBuilder = (function($) {
+	var builders = {};
 
-		
-		var sortableOptions = {};
+	/**
+	 * Generates a function that can be used to call a function in the
+	 * current object context
+	 */
+	function rFunc(func, context, include) {
+		// Create an array out of the other pass arguments
+		var a = Array.prototype.slice.call(arguments);
+		// Shift to remove func
+		a.shift();
+		// Shift to remove context
+		a.shift();
+		// Shift to remove include
+		a.shift();
+		return function () {
+			/**
+			 * Append the arguments from the function call to the arguments
+			 * given when rFunc was called.
+			 */
+			if (include) {
+				a = a.concat(Array.prototype.slice.call(arguments));
+			}
+			func.apply(context, a);
+		};
+	}
 
-		function sortableStop(e, ui) {
-			// Find the item
-			var i;
-			var type = ui.item.attr('data-type');
-			var element = ui.item.attr('data-element');
-			var item = builders[id]['elements'][type]['elements'][element];
-			console.log(type);
-			console.log(element);
-			console.log(item);
-			if (!ui.item.attr('data-drawn')) {
-				if (item['draw']) {
-					var pads = item['draw'](ui.item);
-					if (pads) {
-						console.log(pads);
-						for (i in pads) {
-							console.log('making ' + i + 'sortable');
-							pads[i].sortable(sortableOptions);
-						}
-						builders[id]['store'].find('.item').draggable('option', 'connectToSortable', $('.pad'));
-						builders[id]['store'].find('.item').attr('data-found', 'found');
-						$('.pad').attr('data-found', 'found');
+	function sortableStop(id, e, ui) {
+		// Find the item
+		var i;
+		var type = ui.item.attr('data-type');
+		var element = ui.item.attr('data-element');
+		var elements = builders[id]['elements'][type];
+		var item = elements['elements'][element];
+		console.log(type);
+		console.log(element);
+		console.log(item);
+		if (!ui.item.attr('data-drawn')) {
+			// Find draw function
+			var draw = (item['draw'] ? item['draw'] : (elements['draw'] ? elements['draw'] : null));
+			if (draw) {
+				var pads = draw(ui.item, id);
+				if (pads) {
+					console.log(pads);
+					for (i in pads) {
+						console.log('making ' + i + 'sortable');
+						pads[i].sortable(builders[id].sortableOptions);
 					}
-					ui.item.attr('data-drawn', true);
 				}
+				ui.item.attr('data-drawn', true);
+			} else { // Draw using internal function
+				ui.item.attr('data-value', item['value']);
 			}
 		}
 
-		sortableOptions['stop'] = sortableStop;
+		// Reconnect pads
+		connectPads(id);
 
-		function populateStore(id) {
-			if (builders[id]) {
-				var e, type, i, item;
-				var z, y, x, w;
-				for (e in builders[id]['elements']) {
-					type = builders[id]['elements'][e];
-					if (!type['elements']) {
+		// Parse object
+		if (builders[id].input) {
+			builders[id].input.val(ObjectBuilder.getObjectJSON(id));
+		}
+
+		// Run onchange if we have one
+		if (builders[id].options.onchange) {
+			builders[id].options.onchange(id);
+		}
+	}
+
+	function populateStore(id) {
+		if (builders[id]) {
+			var e, type, i, item, part;
+			var z, y, x, w;
+			for (e in builders[id]['elements']) {
+				type = builders[id]['elements'][e];
+				if (!type['elements']) {
+					continue;
+				}
+
+				if (type['label']) {
+					builders[id]['store'].append((z = $(document.createElement('label'))));
+					z.html(type['label']);
+				}
+				builders[id]['store'].append((z = $(document.createElement('div'))));
+				builders[id].stores[e] = z;
+				for (i in type['elements']) {
+					item = type['elements'][i];
+
+					z.append((y = $(document.createElement('div'))));
+					y.addClass('item');
+					if (part = (item['className'] ? item['className'] : (type['className'] ? type['className'] : false))) {
+						y.addClass(part);
+					}
+					y.attr('data-type', e);
+					y.attr('data-element', i);
+					y.html((item['label'] ? item['label'] : i));
+				}
+			}
+		}
+	}
+
+	function connectPads(id) {
+		console.log('connecting pads');
+		if (builders[id]) {
+			console.log('valid builder');
+			var i, storeTypes = {all: $()};
+
+			for (i in builders[id].pads) {
+				console.log('Checking pad');
+				// Check if is full
+				var max = (builders[id].pads[i].multiple ? builders[id].pads[i].multiple : 1);
+				console.log('Pad has a limit of ' + max);
+				if (max !== true) {
+					console.log('Currently has ' + builders[id].pads[i].pad.children('.item').length);
+					if (builders[id].pads[i].pad.children('.item').length >= max) {
+						console.log('Pad is full');
 						continue;
 					}
-
-					if (type['label']) {
-						builders[id]['store'].append((z = $(document.createElement('label'))));
-						z.html(type['label']);
-					}
-					builders[id]['store'].append((z = $(document.createElement('div'))));
-
-					for (i in type['elements']) {
-						item = type['elements'][i];
-
-						z.append((y = $(document.createElement('div'))));
-						y.addClass('item');
-						y.attr('data-type', e);
-						y.attr('data-element', i);
-						y.html((item['label'] ? item['label'] : i));
-					}
+				}
+				if (!builders[id].pads[i].types) {
+					console.log('Adding pad to all selector');
+					builders[id].pads[i].pad.attr('data-selector', 'all');
+					storeTypes.all = storeTypes.all.add(builders[id].pads[i].pad);
 				}
 			}
+
+			for (i in builders[id].stores) {
+				console.log('connecting store ' + i);
+				// Add pads that accept all to the specific pads
+				if (!storeTypes[i]) {
+					console.log('no specifics, adding all');
+					storeTypes[i] = storeTypes.all;
+				} else {
+					console.log('appending all');
+					storeTypes[i].add(storeTypes.all);
+				}
+
+				storeTypes[i].attr('data-' + i, 'connected');
+				builders[id].stores[i].find('.item').draggable('option', 'connectToSortable', storeTypes[i]);
+				builders[id].stores[i].find('.item').attr('data-added', 'added');
+			}
 		}
+	}
 
-		builders[id] = {
-			'obj': obj,
-			'elements': elements,
-			'options': options,
-		};
+	return {
+		create: function(obj, elements, options) {
+			var z, pad = {};
 
-		// Create divs
-		obj.append((builders[id]['store'] = $(document.createElement('div'))));
-		builders[id]['store'].addClass('store')	
-		obj.append((builders[id]['pad'] = $(document.createElement('div'))));
-		builders[id]['pad'].addClass('pad');
-		builders[id]['pad'].sortable(sortableOptions);
+			id = (new Date().getTime()).toString(16);
+			
+			builders[id] = {
+				obj: obj,
+				store: null, // Stores the store jQuery DOM container
+				stores: {}, // Stores the jQuery DOM container for each type of element
+				elements: elements,
+				input: null,
+				options: options,
+				pad: null, // Stores the pad jQuery DOM container used to start the object build process
+				pads: [],
+				removalIntent: false, // Used to remove determine whether an item should be removed from the object
+				sortableOptions: {
+					over: function (event, ui) {
+						builders[id].removalIntent = false;
+						if (!ui.item.hasClass('remove')) {
+							ui.item.addClass('remove');
+						}
+					},
+					out: function (event, ui) {
+						builders[id].removalIntent = true;
+						if (ui.item.hasClass('remove')) {
+							ui.item.removeClass('remove');
+						}
+					},
+					beforeStop: function (event, ui) {
+						if(builders[id].removalIntent == true){
+							ui.item.remove();   
+						} else if (ui.item.hasClass('remove')) {
+							ui.item.removeClass('remove');
+						}
+					},
+					stop: rFunc(sortableStop, this, true, id)
+				}
+			};
 
-		// Populate the store
-		populateStore(id);
+			if (options.input) {
+				builders[id].input = options.input;
+			}
 
-		builders[id]['store'].find('.item').draggable({
-			appendTo: obj,
-			helper: 'clone',
-			connectToSortable: builders[id]['pad']
-		});
+			// Create divs
+			obj.append((builders[id]['store'] = $(document.createElement('div'))));
+			builders[id]['store'].addClass('store');
+
+			builders[id]['pad'] = this.createPad(id, obj, builders[id].options);
+
+			// Populate the store
+			populateStore(id);
+
+			builders[id]['store'].find('.item').draggable({
+				appendTo: obj,
+				helper: 'clone',
+				connectToSortable: builders[id]['pad']
+			});
+
+			// Connect pads
+			connectPads(id);
+
+			return id;
+		},
+		
+		parseObject: function(id) {
+			if (builders[id]) {
+				return ObjectBuilder.parsePadObject(id, builders[id]['pad']);
+			}
+		},
+
+		parsePadObject: function(id, pad) {
+			if (builders[id] && pad) {
+				var e, obj = [], elements = builders[id].elements;
+
+				pad.children('[data-element]').each(function() {
+					// Check for a getObject function
+					var type = $(this).attr('data-type');
+					var element = $(this).attr('data-element');
+					if (elements[type] && elements[type].elements[element]) {
+						var parseObject 
+						if (parseObject = (elements[type].elements[element].parseObject ? elements[type].elements[element].parseObject : (elements[type].parseObject ? elements[type].parseObject : null))) {
+							obj.push(parseObject(type, element, $(this), id));
+						} else {
+							obj.push({
+								type: type,
+								element: element,
+							});
+						}
+					}
+				});
+
+				return obj;
+			}
+		},
+		
+		getObjectJSON: function (id) {
+			if (builders[id]) {
+				return JSON.stringify(ObjectBuilder.parseObject(id));
+			}
+		},
+		
+		createPad: function(id, obj, options) {
+			var pad = {};
+
+			console.log('Creating pad for ' + id);
+
+			if (builders[id]) {
+				obj.append((pad.pad = $(document.createElement('div'))));
+				pad.pad.addClass('pad');
+				pad.pad.sortable(builders[id].sortableOptions);
+				if (options.types) {
+					pad.types = options.types;
+				}
+				if (options.multiple) {
+					pad.multiple = options.multiple;
+				}
+				builders[id].pads.push(pad);
+
+				return pad.pad;
+			}
+		},
 	};
-})();
+})(jQuery);
